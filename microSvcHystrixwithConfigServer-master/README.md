ea39eaa59674a0efff73f6e9fe1aadfde1e8fa31
Mac LAB <maclab@MACPC270508.local>
2018-07-26 23:16:20 +0530

    second commit

---
+++ microSvcHystrixwithConfigServer-master/README.md
@@ -0,0 +1,52 @@
+# Movie Fun!
+
+## Build
+
+### Build JAR
+
+```bash
+$ ./gradlew clean assemble
+```
+
+### Run Smoke Tests
+
+```bash
+./gradlew test
+```
+
+Smoke Tests require server running on port 8080 by default. To run against a custom URL run
+```bash
+$ MOVIE_FUN_URL=http://moviefun.example.com ./gradlew test
+```
+
+## Deploying to PCF
+
+After running the build, deploy the application with
+```bash
+./gradlew deploy
+```
+
+Reset the entire PCF environment with
+```bash
+./gradlew resetPcfEnv
+```
+
+## Migrations
+
+Create databases needed for local development with
+
+```bash
+mysql -uroot < databases/create_databases.sql
+```
+
+Run local migrations with
+
+```bash
+./gradlew flywayMigrate
+```
+
+Run migrations on the PCF environment which you're logged in to with
+
+```bash
+CF_MIGRATE=true ./gradlew cfMigrate
+```
---
+++ microSvcHystrixwithConfigServer-master/components/movie-fun-api/src/main/java/org/superbiz/moviefun/albumsapi/AlbumsController.java
@@ -0,0 +1,70 @@
+package org.superbiz.moviefun.albumsapi;
+
+import org.apache.tika.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.superbiz.moviefun.blobstore.Blob;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+@Controller
+@RequestMapping("/albums")
+public class AlbumsController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final AlbumsClient albumsClient;
+    private final CoverCatalog coverCatalog;
+
+    public AlbumsController(AlbumsClient albumsClient, CoverCatalog coverCatalog) {
+        this.albumsClient = albumsClient;
+        this.coverCatalog = coverCatalog;
+    }
+
+    @GetMapping
+    public String index(Map<String, Object> model) {
+        model.put("albums", albumsClient.getAlbums());
+        return "albums";
+    }
+
+    @GetMapping("/{albumId}")
+    public String details(@PathVariable long albumId, Map<String, Object> model) {
+        model.put("album", albumsClient.find(albumId));
+        return "albumDetails";
+    }
+
+    @PostMapping("/{albumId}/cover")
+    public String uploadCover(@PathVariable Long albumId, @RequestParam("file") MultipartFile uploadedFile) {
+        if (uploadedFile.getSize() > 0) {
+            try {
+                coverCatalog.uploadCover(albumId, uploadedFile.getInputStream(), uploadedFile.getContentType());
+            } catch (IOException e) {
+                logger.warn("Error while uploading album cover", e);
+            }
+        }
+
+        return format("redirect:/albums/%d", albumId);
+    }
+
+    @GetMapping("/{albumId}/cover")
+    public HttpEntity<byte[]> getCover(@PathVariable long albumId) throws IOException, URISyntaxException {
+        Blob coverBlob = coverCatalog.getCover(albumId);
+
+        byte[] imageBytes = IOUtils.toByteArray(coverBlob.inputStream);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(coverBlob.contentType));
+        headers.setContentLength(imageBytes.length);
+
+        return new HttpEntity<>(imageBytes, headers);
+    }
+}
