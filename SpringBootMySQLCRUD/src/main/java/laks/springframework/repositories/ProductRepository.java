c02bc58bc01c19740e89f5d5a47a5d4787e1caba
Mac LAB <maclab@MACPC270508.local>
2018-07-26 10:26:51 +0530

    first commit

---
+++ SpringBootMySQLCRUD/src/main/java/laks/springframework/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package laks.springframework.repositories;
+
+import laks.springframework.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
